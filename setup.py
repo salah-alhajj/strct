b910116183bc8c2999dab442d3cf94345d841a0c
salah-alhajj <contact@salahaldain.com>
2024-08-01 18:57:29 +0300

    v0.4.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="strct-tool",
-    version="0.4.4",
+    version="0.4.5",
     author="Salah alhajj",
     author_email="contact@contact.com",
     description="A tool for creating project structures from templates",
